4ec61b501742b2f7941e278d9e1aa35633affc78
Ramond Zhou <2200011057@stu.pku.edu.cn>
2024-05-15 21:18:15 +0800

    added a shell script for preparing dataset.

---
+++ prepare_dataset.sh
@@ -0,0 +1,5 @@
+mkdir data
+wget https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip
+unzip modelnet40_ply_hdf5_2048.zip
+mv modelnet40_ply_hdf5_2048 data
+rm modelnet40_ply_hdf5_2048.zip
